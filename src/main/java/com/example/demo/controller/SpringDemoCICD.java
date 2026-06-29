b50c82b085ab401330572420b724cde6fd9b62fd
Sureshgangula8 <82363604+Sureshgangula8@users.noreply.github.com>
2026-06-29 13:50:18 +0530

    first commit

---
+++ src/main/java/com/example/demo/controller/SpringDemoCICD.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/testcicd")
+public class SpringDemoCICD {
+
+	@GetMapping
+	public String demo() {
+		return "Welcome to Spring boot demo for CI / CD --- Jenkin.";
+	}
+}
